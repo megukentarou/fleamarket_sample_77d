19a7a16466e5282dc0b57fff09808e3b50b0b95b
supertaiga222 <66627988+supertaiga222@users.noreply.github.com>
2020-08-16 15:56:13 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "fleamarket_sample_77d"
-set :repo_url, "git@example.com:supertaiga222/fleamarket_sample_77d.git"
+set :repo_url, "git@github.com:supertaiga222/fleamarket_sample_77d.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
