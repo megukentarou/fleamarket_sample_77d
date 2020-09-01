d5b1add4aa03f94418dc455abef651eba6fd21ae
megukentarou <megukentarou@gmail.com>
2020-09-01 14:55:18 +0900

    ルーティングのonly追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items
-  resources :users
+  resources :items, only: [:index, :new]
+  resources :users, only: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
