48ec10269a8497cc27f691f078c98babe6776127
megukentarou <megukentarou@gmail.com>
2020-09-06 22:10:32 +0900

    イベント発火とバリューの取り出し

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -2,7 +2,16 @@ window.addEventListener("load", function() {
   let parent = document.querySelector("select#parent");
   console.log(parent)
   parent.addEventListener("change", function() {
-    console.log("親カテゴリーが選択されました");
-    alert("親カテゴリーが選択されました");
+    let parentValue = parent.value;
+    console.log(parentValue)
+    if(parentValue === "選択して下さい") {
+      parent.addEventListener("blur", function() {
+        console.log("フォーカスから離れました");
+        // 子カテゴリーのセレクトタグを隠す実装が必要
+      })
+    } else {
+      console.log("次は子カテゴリーを選択して下さい");
+      
+    }
   });
 });
---
+++ app/assets/javascripts/new.js
@@ -0,0 +1,8 @@
+// $(function(){
+//   $("#parent").on("change", function(e){
+//     e.preventDefault();
+//     console.log(this);
+//     let parentData = new FormData(this);
+//     let url = $(this).attr("value");
+//   })
+// });
