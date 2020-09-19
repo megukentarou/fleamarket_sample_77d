5567591c3a813dbb26e51e5a0c3d4180b7034f39
megukentarou <megukentarou@gmail.com>
2020-09-19 14:13:56 +0900

    画像の連続投稿とビューの表示完了

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,76 +1,91 @@
-$(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
-  const buildFileField = (num)=> {
-    console.log("フォームの追加")
-    const html = `<input value="" class="file_field" type="file" name="item[images_attributes][${num}][src]" id="item_images_attributes_${num}_src">`;
-    return html;
-  }
+$(document).on('input', '.image_upload', function(){
+      var preview = $(`<div class="image-preview__wrapper">
+                        <img class="preview">
+                        <div class="image-preview_btn"><div class="image-preview_btn_edit">編集</div>
+                        <div class="image-preview_btn_delete">削除</div>
+                        </div>
+                      </div>`); 
+      //次の画像を読み込むためのinput。処理の最後にappendで追加する。 
+      var append_input = $(`<li class="input">
+                              <label class="upload-label">
+                                <div class="items__image__upload__comment">
+                                  <i class="fas fa-camera"></i>
+                                  <p>ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード</p>
+                                  <div class="input-area">
+                                    <input class="hidden image_upload" type="file">
+                                  </div>
+                                </div>
+                              </label>
+                            </li>`)
 
-  $(document).on("click", ".file_field", function() {
-  // 素材を表示
-      $ul = $("#previews");
-      $li = $(this).parents("li");
-      $label = $(this).parents(".items__image__upload");
-      $label.css("display", "none");
-      $inputs = $ul.find(".file_field");
-      // $liに追加する為のプレビュー画面のHTML
-      var preview = $(
-        `<div class="image-preview__wrapper">
-          <img class="preview">
-        </div>
-        <div class="image-preview_btn">
-          <div class="image-preview_btn_edit">編集</div>
-          <div class="image-preview_btn_delete">削除</div>
-        </div>`
-      );
-      $(preview).find(".preview").attr("src", e.target.result);
-      // <img class="preview">をfindで指定
-      // attrでimgタグにsrcを挿入
+      //こちらは5枚目と10枚目の投稿時のpタグの横幅を調整している。それ以外は同じ
+      var append_input2 = $(`<li class="input">
+                              <label class="upload-label">
+                                <div class="items__image__upload__comment">
+                                  <i class="fas fa-camera"></i>
+                                  <p>ドラッグアンド<br>ドロップ<br>またはクリックして<br>ファイルをアップロード</p>
+                                  <div class="input-area">
+                                    <input class="hidden image_upload" type="file">
+                                  </div>
+                                </div>
+                              </label>
+                            </li>`)
 
-      // 次の画像を読み込む為のinput 処理の最後にappendで追加する
-      var append_input = $(
-        `<li class="input">
-            <label class="ipload-label">
-              <div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード
-                <div class="input-area">
-                  <input class="hidden image_upload" type="file">
-                </div>
-              </div>
-            </label>
-          </li>`
-      );  
-  })
+      //素材
+      $ul = $('#previews')
+      $li = $(this).parents('li');
+      $label = $(this).parents('.upload-label');
+      $inputs = $ul.find('.image_upload');
+
+      //処理
+      //inputに画像を読み込んだら、"プレビューの追加"と"新しいli追加"処理が動く
+      $('.image_upload').on('change', function (e) {
+        //画像URLを取得
+        var reader = new FileReader();
+        //プレビューに追加させるために、inputから画像ファイルを読み込む
+        reader.readAsDataURL(e.target.files[0]);
   
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // 処理
-  // 画像をファイル選択して中身が変更（追加）されたらイベント発火
-  $('.file_field').on('change', function(e) {
-      // 画像のURLを取得
-      var reader = new FileReader();
-      // プレビューに追加させる為に、inputから画像ファイルを読み込む
-      reader.readAsDataURL(e.target.files[0]);
-      console.log(reader);
+        //画像URLをimgに追加
+        //画像ファイルが読み込んだら、処理が実行される。 
+        reader.onload = function(e){
+          //previewをappendで追加する前に、プレビューできるようにinputで選択した画像を<img>に'src'で付与させる
+          $(preview).find('.preview').attr('src', e.target.result);
+        }
+  
+        //inputを保有するliにimgを追加
+        $li.append(preview);
+      });
 
-      // 画像URLをimgに追加
-      // 画像ファイルを読み込んだら、処理が実行される
-      reader.onload = function(e) {
-        $(preview).find(".preview").attr("src", e.target.result);
-      }
-      
-      // inputを保有する、liにimgを追加
-      $li.append(preview);
+        //プレビュー完了後は、inputを非表示にさせる。これによりプレビューだけが残る。
+        $label.css("display", "none");
+        $li.removeClass("input");
+        $li.addClass("image-preview");
+        $lis = $ul.find(".image-preview");
 
-      // fileIndexの先頭の数字を使ってinputを作る
-      // $('.items__image__upload__comment').append(buildFileField(fileIndex[0]));
-      // fileIndex.shift();
-      // // 末尾の数に1足した数を追加する
-      // fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-  });
-
-  // $('items__image__upload__comment').on('click', '.js-remove', function() {
-  //   $(this).parent().remove();
-  //   // 画像入力欄が0個にならないようにしておく
-  //   if ($('.js-file').length == 0) $('items__image__upload__comment').append(buildFileField(fileIndex[0]));
-  // });
+        //"ul"に新しい"li(inputボタン)"を追加させる。
+        if ($lis.length < 4) {
+          $ul.append(append_input);
+        } else if ($lis.length == 4) {
+          $li.addClass("image-preview");
+          $ul.append(append_input2);
+        } else if ($lis.length == 5) {
+          $li.addClass("image-preview");
+          $ul.append(append_input);
+        } else if ($lis.length < 9) {
+          $li.addClass("image-preview");
+          $ul.append(append_input);
+        } else if ($lis.length == 9) {
+          $li.addClass("image-preview");
+          $ul.append(append_input2);
+        }
+        //inputの最後の"data-image"を取得して、input nameの番号を更新させてる。
+      $inputs.each(function (num, input) {
+        //nameの番号を更新するために、現在の番号を除去
+        $(input).removeAttr("name");
+        $(input).attr({
+          id: "product_images_attributes_" + num + "_name",
+          name: "product[images_attributes][" + num + "][name]",
+        });
+        console.log(input);
+      });
 });
