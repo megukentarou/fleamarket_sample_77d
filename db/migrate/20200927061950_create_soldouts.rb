7cc33b104affb80db2ba9c330823bd9ccba692c9
sannnann131 <tetsu09221991@gmaill.com>
2020-09-27 15:28:42 +0900

    売却済みテーブルの作成

---
+++ db/migrate/20200927061950_create_soldouts.rb
@@ -0,0 +1,9 @@
+class CreateSoldouts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :soldouts do |t|
+      t.integer :user_id,        null: false, foreign_key: true
+      t.integer :item_id,        null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/soldouts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :soldout do
+    
+  end
+end
