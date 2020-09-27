8377564ab0401b1223179d3f9b2bda9abfc28dea
supertaiga222 <66627988+supertaiga222@users.noreply.github.com>
2020-09-27 16:01:15 +0900

    Update residency.rb

--- app/models/residency.rb
+++ app/models/residency.rb
@@ -10,7 +10,7 @@ class Residency < ApplicationRecord
     validates :address
     validates :family_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
     validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
-validates :family_name_reading,  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
-validates :first_name_reading,  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+    validates :family_name_reading,  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+    validates :first_name_reading,  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
   end
 end
