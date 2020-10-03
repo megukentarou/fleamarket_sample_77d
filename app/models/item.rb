3bda7953ca8c313c84fc5b96f2b4601a7ee53e43
megukentarou <megukentarou@gmail.com>
2020-10-03 21:47:20 +0900

    エラーハンドリングの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,17 +6,22 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
   has_one :soldout
 
   #以下はフリマのバリデーションコード
   with_options presence: true do
-    validates :name,               length: {maximum: 40}
-    validates :price,              length: {maximum: 7}
-    validates :explanation,        length: {maximum: 1000}
+    validates :name,
+      length: {maximum: 40}
+    validates :price,
+      length: {maximum: 7}
+    validates :explanation,
+      length: {maximum: 1000} 
     validates :delivery_day_id 
     validates :fee_id
     validates :condition_id 
     validates :prefecture_id
+    validates :images
+    validates :category_id
   end
 end
