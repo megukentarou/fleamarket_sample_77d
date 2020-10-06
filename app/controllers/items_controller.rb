0f66b183768fb991df0394075aa4fd6c2ee46f50
megukentarou <megukentarou@gmail.com>
2020-10-06 11:18:15 +0900

    update branch

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,11 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # @item = Item.find(params[:id])
   end
   
   def edit
-    # @item = Item.find(params[:id])
   end
 
   def update
